84bae870dd64c7c2c17870bcf8a91a6d2e8c22ef
Johannes <johannes.programming@gmail.com>
2025-08-19 01:34:40 +0200

    Version 1.0.1+local

--- src/datarepr/core.py
+++ src/datarepr/core.py
@@ -5,11 +5,12 @@ __all__ = ["datarepr"]
 
 def datarepr(name: Any, /, *args: Any, **kwargs: Any) -> str:
     "This function allows for common sense representation."
-    parts = list()
-    for a in args:
-        parts.append(repr(a))
-    for i in kwargs.items():
-        parts.append("%s=%r" % i)
-    content = ", ".join(parts)
-    ans = "%s(%s)" % (name, content)
+    parts: list = list()
+    x: Any
+    for x in args:
+        parts.append(repr(x))
+    for x in kwargs.items():
+        parts.append("%s=%r" % x)
+    content: str = ", ".join(parts)
+    ans: str = "%s(%s)" % (name, content)
     return ans
