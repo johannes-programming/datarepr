7593415ee71067e441859ba691ed5fc0cac6f7dc
Johannes <johannes.programming@gmail.com>
2025-10-19 21:48:47 +0200

    Version 1.1.0

--- src/datarepr/core.py
+++ src/datarepr/core.py
@@ -1,6 +1,6 @@
 from typing import *
 
-__all__ = ["datarepr"]
+__all__ = ["datarepr", "oxford"]
 
 
 def datarepr(name: Any, /, *args: Any, **kwargs: Any) -> str:
@@ -13,4 +13,19 @@ def datarepr(name: Any, /, *args: Any, **kwargs: Any) -> str:
         parts.append("%s=%r" % x)
     content: str = ", ".join(parts)
     ans: str = "%s(%s)" % (name, content)
+    return ans
+
+
+def oxford(*args: Any, conj: Any = "and", default: Any = "") -> Any:
+    if len(args) == 0:
+        return default
+    ans: str
+    if len(args) == 1:
+        ans = "%r"
+    elif len(args) == 2:
+        ans = f"%r {conj} %r"
+    else:
+        ans = "%r, " * (len(args) - 1)
+        ans += f"{conj} %r"
+    ans %= args
     return ans
