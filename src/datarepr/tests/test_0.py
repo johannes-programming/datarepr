4d7328c610884eef6803cdaa0ba6654d344ca77f
Johannes <johannes.programming@gmail.com>
2025-10-19 21:48:47 +0200

    Version 1.1.0

--- src/datarepr/tests/test_0.py
+++ src/datarepr/tests/test_0.py
@@ -1,7 +1,7 @@
 import unittest
 from typing import *
 
-from datarepr.core import datarepr
+from datarepr.core import datarepr, oxford
 
 
 class TestDatareprFunction(unittest.TestCase):
@@ -85,6 +85,16 @@ class TestDatareprFunction(unittest.TestCase):
         "Test with no arguments, not even the name."
         with self.assertRaises(TypeError):
             datarepr()  # Should raise a TypeError due to missing required 'name' argument.
+
+
+class TestOxfordFunction(unittest.TestCase):
+    def test_no_args_at_all(self: Self) -> None:
+        self.assertEqual("", oxford())
+        self.assertEqual(42, oxford(default=42))
+        self.assertEqual("1, 'two', and 3", oxford(1, "two", 3))
+        self.assertEqual("1, 'two', and 3", oxford(1, "two", 3, default=42))
+        self.assertEqual("1 and 'two'", oxford(1, "two"))
+        self.assertEqual("1 or 'two'", oxford(1, "two", conj="or"))
 
 
 if __name__ == "__main__":
