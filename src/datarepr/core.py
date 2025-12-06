21fa9d4b0ebdc4bb4e9611bf7973b4016ae44b17
Johannes <johannes-programming@mailfence.com>
2025-12-06 16:44:15 +0100

    Version 1.0.0

--- src/datarepr/core.py
+++ src/datarepr/core.py
@@ -4,7 +4,7 @@ __all__ = ["datarepr"]
 
 
 def datarepr(name: Any, /, *args: Any, **kwargs: Any) -> str:
-    "Common sense representation."
+    """Common sense representation."""
     parts = list()
     for a in args:
         parts.append(repr(a))
