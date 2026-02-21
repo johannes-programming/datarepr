51f07f827da37a5249b4ce956b4c7ed1f5e9ccf5
Johannes <johannes.programming@gmail.com>
2026-02-21 23:10:19 +0100

    Version 1.1.4

--- src/datarepr/core.py
+++ src/datarepr/core.py
@@ -5,21 +5,20 @@ __all__ = ["datarepr", "oxford"]
 
 def datarepr(name: Any, /, *args: Any, **kwargs: Any) -> str:
     "This function allows for common sense representation."
-    parts: list = list()
-    x: Any
-    for x in args:
-        parts.append(repr(x))
-    for x in kwargs.items():
-        parts.append("%s=%r" % x)
-    content: str = ", ".join(parts)
-    ans: str = "%s(%s)" % (name, content)
-    return ans
+    content: str
+    item: tuple[Any, Any]
+    parts: list
+    parts = list(map(repr, args))
+    for item in kwargs.items():
+        parts.append("%s=%r" % item)
+    content = ", ".join(parts)
+    return "%s(%s)" % (name, content)
 
 
 def oxford(*args: Any, conj: Any = "and", default: Any = "") -> Any:
+    ans: str
     if len(args) == 0:
         return default
-    ans: str
     if len(args) == 1:
         ans = "%r"
     elif len(args) == 2:
